1733eee75c667a2f5147238fe6dc3ca961a93c44
tyler <tyler@cruftbusters.com>
2020-12-30 19:18:23 -0600

    refactor

--- src/main/groovy/com/cruftbusters/tree2table/TreeToTable.groovy
+++ src/main/groovy/com/cruftbusters/tree2table/TreeToTable.groovy
@@ -2,15 +2,29 @@ package com.cruftbusters.tree2table
 
 class TreeToTable {
   List<List> convert(List<Map> tree) {
-    List<List> rows = []
-    for (Map node : tree){
-      (rows.size()..<getDepth(node)).each {rows.add([])}
-      rows[0] += unlinkChildren(node)
-      if (node.children) {
-        rows[1] += node.children
-      }
+    joinSlices(tree.collect(this.&makeSlice))
+  }
+
+  private List<List> joinSlices(List slices) {
+    int maxHeight = slices*.size().max()
+    slices = slices.collect{this.&setHeight(it, maxHeight)}
+    (0..<maxHeight).collect{height-> slices*.getAt(height).flatten()}
+  }
+
+  private static List<List> makeSlice(Map node) {
+    List<List<Map>> rows = (0..<getDepth(node)).collect {[]}
+    rows[0] += unlinkChildren(node)
+    if (node.children) {
+      rows[1] += node.children
     }
     rows
+  }
+
+  private static List setHeight(List slice, int height) {
+    [].tap {next->
+      addAll(slice)
+      (size()..<height).each{add([])}
+    }
   }
 
   private static Map unlinkChildren(Map node) {
