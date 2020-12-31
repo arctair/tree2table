90ab7d3470ce9ff86ae4096c054545975fa0ff08
tyler <tyler@cruftbusters.com>
2020-12-30 18:43:44 -0600

    refactor

--- src/main/groovy/com/cruftbusters/tree2table/TreeToTable.groovy
+++ src/main/groovy/com/cruftbusters/tree2table/TreeToTable.groovy
@@ -2,6 +2,20 @@ package com.cruftbusters.tree2table
 
 class TreeToTable {
   List<List> convert(List<Map> tree) {
-    [tree]
+    List<List> rows = []
+    for (Map node : tree){
+      if (rows.size() < 1) {
+        rows.add([])
+      }
+      rows[0] += unlinkChildren(node)
+    }
+    rows
+  }
+
+  private static Map unlinkChildren(Map node) {
+    [:].tap {
+      it.putAll(node)
+      it.remove('children')
+    }
   }
 }
